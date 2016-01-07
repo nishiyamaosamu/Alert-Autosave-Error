6a7878198727c95fcc1ef4e6f5a25fc9df7e9807
Osamu Nishiyama <osamuintz@gmail.com>
2016-01-07 09:32:48 +0900

    Change to Class

--- alert-autosave-error/alert-autosave-error.php
+++ alert-autosave-error/alert-autosave-error.php
@@ -4,32 +4,39 @@ Plugin Name: Alert Autosave Error
 Plugin URI: https://github.com/nishiyamaosamu/Alert-Autosave-Error
 Description: Avoid losing data after nonces expire
 Author: Osamu Nishiyama
-Version: 0.1
+Version: 0.2
 Author URI: https://github.com/nishiyamaosamu
 */
 
-add_action( 'plugins_loaded', 'my_plugins_loaded' );
+add_action( 'plugins_loaded', array( 'Alert_Autosave_Error', 'get_object' ) );
 
-/**
- * Fires on plugins_loaded hook.
- */
-function my_plugins_loaded()
-{
-    add_action( 'admin_enqueue_scripts', 'admin_enqueue_scripts' );
-}
 
-/**
- * Fires on admin_enqueue_scripts hook
- */
-function admin_enqueue_scripts( $hook_suffix )
-{
-    if ( 'post-new.php' === $hook_suffix || 'post.php' === $hook_suffix ) {
-        wp_enqueue_script(
-            'alert-autosave-error',
-            plugins_url( 'js/alert-autosave-error.js', __FILE__ ),
-            array( 'jquery' ),
-            filemtime( dirname( __FILE__ ) . '/js/alert-autosave-error.js' ),
-            true
-        );
+class Alert_Autosave_Error {
+
+    public static function get_object() {
+        static $instance;
+        if ( NULL === $instance ) {
+            $instance = new self();
+        }
+        return $instance;
+    }
+
+    private function __construct() {
+        if ( ! is_admin() ) {
+            return;
+        }
+        add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts') );
+    }
+
+    public function admin_enqueue_scripts( $hook_suffix ) {
+        if ( 'post-new.php' === $hook_suffix || 'post.php' === $hook_suffix ) {
+            wp_enqueue_script(
+                'alert-autosave-error',
+                plugins_url( 'js/alert-autosave-error.js', __FILE__ ),
+                array( 'jquery' ),
+                filemtime( dirname( __FILE__ ) . '/js/alert-autosave-error.js' ),
+                true
+            );
+        }
     }
 }
